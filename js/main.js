e79adcaf6a6ab780e2677034b914202ded586f80
Humphrey Obuobi <obuobhum@gmail.com>
2016-03-18 11:10:54 -0400

    added type.js support and scrolling links

--- js/main.js
+++ js/main.js
@@ -1,11 +1,42 @@
 $(document).ready(function(){
 $('#spotlight').hide();
+$(function(){
+		$("#type").typed({
+			strings: ["DESIGNER.", "ENGINEER.","LEADER.","RUNNER.","INNOVATOR."],
+			typeSpeed: 0,
+      backDelay: 1000,
+      loop: true
+		});
+	});
+
+/*$(function(){
+  if( ($("#type").text() == 'ENGINEER.') || ($("#type").text() == 'INNOVATOR.')){
+    console.log("VOWELSSSS");
+    $('.banner').first().text('I AM AN');
+  }
+  else { $('.banner').first().text('I AM A') }
+})
+*/
+$(".menu-item").click(function(){
+  var name = "#"+ $(this).text();
+  console.log(name);
+  $('html, body').animate({
+      scrollTop: $(name).offset().top
+  }, 700);
+})
+$(".menu-sig").click(function(){
+  $('html, body').animate({
+      scrollTop: $("body").offset().top
+  }, 700);
+})
+
+
 $('.port-card').click(function(){
   console.log('clickity');
-  $('#spotlight').fadeIn();
+  $('#spotlight').fadeIn(300);
 });
 $('#spotlight').click(function(){
-  $(this).fadeOut();
+  $(this).fadeOut(300);
 })
 $('#menu')
   .mouseenter(function(){
